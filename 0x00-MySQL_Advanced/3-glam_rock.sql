78725e69e365baf47a5bdd6835b523de2d54b818
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-11 19:24:49 +0300

    correction in sql statement

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,6 @@
 -- lists all bands with Glam Rock genre as main style
-SELECT band_name,
-    CASE 
-        WHEN split IS NULL THEN (2022 - formed)
-        ELSE (split - formed)
-        END AS life_span
+SELECT band_name, 
+       (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam Rock%'
-ORDER BY life_span DESC;
+ORDER BY lifespan DESC;
