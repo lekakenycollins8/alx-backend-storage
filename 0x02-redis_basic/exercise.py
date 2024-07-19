c3a6f59f9ba8345676f6397da0f66823fd157c27
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-19 16:11:56 +0300

    gets string and int values via key

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,12 +7,17 @@ The method should generate a random key (e.g. using uuid), store the
 input data in Redis using the random key and return the key.Type-annotate
 store correctly. Remember that data can be a
 str, bytes, int or float.
+create a get method that take a key string argument and an optional Callable argument named fn. This callable will be used to convert the data back to the desired format.
+Remember to conserve the original Redis.get behavior if the key does not exist.
+Also, implement 2 new methods: get_str and get_int that will automatically parametrize Cache.get with the correct conversion function.
 """
 
 
 import redis
 import uuid
-from typing import Any
+from typing import Union
+
+
 
 
 class Cache:
@@ -20,7 +25,29 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """generates and returns random key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """get data from redis"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """get data from redis and return as string"""
+        data = self._redis.get(key)
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """get data from redis and return as int"""
+        data = self._redis.get(key)
+        try:
+            data = int(data.decode('utf-8'))
+        except Exception:
+            data = 0
+        return data
