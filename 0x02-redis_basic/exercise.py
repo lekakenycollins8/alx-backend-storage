e4d06b647e6981a0c0b3f5f07db16d61f2ce4efb
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-19 16:20:44 +0300

    increments key

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,21 +10,38 @@ str, bytes, int or float.
 create a get method that take a key string argument and an optional Callable argument named fn. This callable will be used to convert the data back to the desired format.
 Remember to conserve the original Redis.get behavior if the key does not exist.
 Also, implement 2 new methods: get_str and get_int that will automatically parametrize Cache.get with the correct conversion function.
+In this task, we will implement a system to count how many times methods of the Cache class are called.
+
+Above Cache define a count_calls decorator that takes a single method Callable argument and returns a Callable.
+
+As a key, use the qualified name of method using the __qualname__ dunder method.
+
+Create and return function that increments the count for that key every time the method is called and returns the value returned by the original method.
 """
 
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
-
-
+def count_calls(method: Callable) -> Callable:
+    """count calls decorator"""
+    key = method.__qualname__
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
+    """creates a cache class with redis instance"""
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generates and returns random key"""
         key = str(uuid.uuid4())
